ecdbfd5502aef3eb2054854c607390fae2b642b5
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2025-04-25 12:43:47 +0500

    начала писать первую часть домашки

--- js/modalBigPic.js
+++ js/modalBigPic.js
@@ -1,10 +1,23 @@
-const picturesContainer = document.querySelector('.pictures'); // блок с рандомными фотографиями
+const pictures = document.querySelector('.pictures'); // блок с рандомными фотографиями
 const bigPic = document.querySelector('.big-picture'); // модалка для большого фото
+const smallPic = pictures.querySelector('.picture'); // все маленькие фотки
+const cancelBut = bigPic.querySelector('.cancel'); // крестик в модалке
 
 const toggleClassModal = function () {
   bigPic.classList.toggle('hidden');
 };
 
-picturesContainer.addEventListener('click', () => {
+cancelBut.addEventListener('click', () => {
   toggleClassModal();
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 27) {
+    toggleClassModal();
+  }
+});
+
+pictures.addEventListener('click', () => {
+  toggleClassModal();
+  bigPic.querySelector('.big-picture__img img').src = smallPic.querySelector('.picture__img').src;
 });
