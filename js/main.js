ec4b2022c8d8a9570337d0e14c4cca8c12b2cceb
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2025-04-26 01:49:00 +0500

    доделала первую часть

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-import './util.js';
-import './renderPicMiniature.js';
-import './modalBigPic.js';
+import {createBlockObjects} from './data.js';
+import {renderPicMiniature} from './renderPicMiniature.js';
+import {modalBigPic} from './modalBigPic.js';
+
+
+const dataPictures = createBlockObjects(1, 25);
+renderPicMiniature(dataPictures);
+modalBigPic(dataPictures);
