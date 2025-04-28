a5e4e96d61b031dc90cb36171c88f2e2b2187cd8
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2025-04-29 01:28:26 +0500

    дописала alt для фоток, написала условия отображения кнопок и счетчика

--- js/modalBigPic.js
+++ js/modalBigPic.js
@@ -14,9 +14,13 @@ export const renderModalBigPhoto = (dataPictures) => {
     body.classList.toggle('modal-open');
   };
 
-  const toggleiddenClass = () => {
-    commentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
+  const toggleiddenClass = (evt) => {
+    if (evt < 5) {
+      commentCount.classList.add('hidden');
+      commentsLoader.classList.add('hidden');
+    } else if (evt === 5) {
+      commentsLoader.classList.add('hidden');
+    }
   };
 
   const onDocumentKeydown = (evt) => {
@@ -51,6 +55,7 @@ export const renderModalBigPhoto = (dataPictures) => {
       const commentElement = cloneComment.querySelector('.social__comment');
 
       commentElement.querySelector('img').src = comment.avatar;
+      commentElement.querySelector('img').alt = comment.name;
       commentElement.querySelector('.social__text').textContent = comment.message;
 
       fragment.append(commentElement);
@@ -62,37 +67,34 @@ export const renderModalBigPhoto = (dataPictures) => {
   // В модуле, который отвечает за отрисовку окна с полноразмерным изображением,
   // доработайте код по выводу списка комментариев(пункт 4.6) таким образом,
 
-  // 4.6. Все комментарии к изображению выводятся в блок .social__comments.
+  // 4.6.
   // Сразу после открытия изображения в полноэкранном режиме отображается не более 5 комментариев.
   // Количество показанных комментариев и общее число комментариев отображается в блоке .social__comment-count.
   // Пример разметки списка комментариев приведён в блоке .social__comments.
-  // Комментарий оформляется отдельным элементом списка li с классом social__comment.
-  // Аватарка автора комментария отображается в блоке .social__picture.
-  // Имя автора комментария отображается в атрибуте alt его аватарки.
-  // Текст комментария выводится в блоке .social__text.
 
   // чтобы список показывался не полностью, а по 5 элементов,
   // и следующие 5 элементов добавлялись бы по нажатию на кнопку «Загрузить ещё».(пункт 4.7)
-  // Не забудьте реализовать обновление числа показанных комментариев в блоке .social__comment-count.
 
   // 4.7. Отображение дополнительных комментариев происходит при нажатии на кнопку .comments-loader.
   // При нажатии на кнопку отображается не более 5 новых комментариев.
   // При изменении количества показанных комментариев число показанных комментариев
   // в блоке .social__comment-count также изменяется.
 
+  // Не забудьте реализовать обновление числа показанных комментариев в блоке .social__comment-count.
+
   pictures.forEach((picture) => {
     picture.addEventListener('click', (evt) => {
       openUserModal();
-      toggleiddenClass();
       toggleBodyScrollClass();
       const picId = +evt.currentTarget.getAttribute('data-id');
       const dataPicture = dataPictures.filter((dataPic) => dataPic.id === picId)[0];
       bigPic.querySelector('.big-picture__img img').src = dataPicture.url;
       bigPic.querySelector('.likes-count').textContent = dataPicture.likes;
       bigPic.querySelector('.social__comment-total-count').textContent = dataPicture.comments.length;
+      bigPic.querySelector('.social__caption').textContent = dataPicture.description;
       bigPic.querySelector('.social__comments').innerHTML = '';
       bigPic.querySelector('.social__comments').append(renderComments(dataPicture.comments));
-      bigPic.querySelector('.social__caption').textContent = dataPicture.description;
+      toggleiddenClass(dataPicture.comments.length);
     });
   });
 };
